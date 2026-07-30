d41e123991995fdc1b2faf7440cbe065954bf793
R. Műkhɛthwa <ramantswanamukhethwa16@gmail.com>
2026-07-30 17:47:56 +0200

    Clean up app run block

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -6,5 +7,7 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
+
